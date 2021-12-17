660c3a60b53138cfb24271b298afc2b3612e5437
hyo0803 <90206886+hyo0803@users.noreply.github.com>
2021-12-17 12:38:57 +0300

    Add files via upload

--- sem11_12/script.js
+++ sem11_12/script.js
@@ -15,14 +15,12 @@ var city1 = {
     // 4. ----------------
     exportStr() {
         // метод для первого объекта
-        for (let key in city1) {
-            document.write(i + city1[key] + '<br/>')
-            if (city1[key].includes('(')) {
-                document.write('function')
-            } else {
-                continue;
-            }
-        }
+       return 'name'+'='+ city1.name + '<br/>' + 'population'+'='+city1.population+'<br/>'
+    },
+
+    //5. ---------------
+    getCity(){
+        return getObj.apply(this).exportStr();
     },
 };
 
@@ -40,12 +38,20 @@ var city2 = {
     // 4. ----------------
     exportStr() {
         //метод для второго объекта
-        for (let key in city2) {
-            document.write(key + ' = ' + city2[key] + '<br/>')
-        }
+        return 'name'+'='+ city2.name + '<br/>' + 'population'+'='+city2.population+'<br/>'
+    },
+
+    //5. ---------------
+    getCity(){
+        return getObj.apply(this).exportStr();
     },
 
 };
+
+// 5. ----------------------------------------------
+var getObj = function() {
+    return this;
+    }
 
 // тк мы вводим данные, то используем условие и выводим значение метода
 if (city == "city 1") {
@@ -53,25 +59,61 @@ if (city == "city 1") {
     document.write(city1.getName() + '<br/>');
 
     document.write('<br/>' + 'Задание 4' + '<br/>');
-    city1.exportStr();
+    document.write(city1.exportStr());
+
+    document.write('<br/>' + 'Задание 5' + '<br/>');
+    document.write(city1.getCity() + '<br/>');
 
 } else if (city == "city 2") {
     document.write('<br/>' + 'Задание 3' + '<br/>');
     document.write(city2.getName() + '<br/>');
 
     document.write('<br/>' + 'Задание 4' + '<br/>');
-    city2.exportStr();
+    document.write(city2.exportStr());
+
+    document.write('<br/>' + 'Задание 5' + '<br/>');
+    document.write(city1.getCity() + '<br/>');
 }
 
 
-// 5. ----------------------------------------------
-var getObj = function() {
+// 7.
+var d1 = [45, 78, 10, 3];
+d1[7]=100;
+console.log('Задание 7');
+console.log('Массив: '+d1);
+console.log('Элемент №6: '+d1[6]);
+console.log('Элемент №7: '+d1[7]);
 
-    }
-    // 6.
-    // 7.
-    // 8.
-    // 9.
-    // 10.
-    // 11.
-    // 12.
+// 8.
+var d2 = [45, 78, 10, 3];
+var sum2 = 0;
+for (let i=0; i<d2.length;i++){
+    sum2+=d2[i];
+};
+console.log('Задание 8');
+console.log('Sum2: '+sum2);
+
+document.write('<br/>' + 'Задание 8' + '<br/>');
+document.write('Sum2: '+sum2 + '<br/>');
+
+// 9.
+var d3 = [45, 78, 10, 3];
+d3[7] = 100;
+var sum3 = 0;
+for ( let key in d3) {
+    sum3 += d3[key];
+};
+console.log('Задание 9');
+console.log('Sum3: '+sum3);
+
+document.write('<br/>' + 'Задание 9' + '<br/>');
+document.write('Sum3: '+sum3 + '<br/>');
+
+// 10.
+var d4 = [45, 78, 10, 3];
+function my(a,b){
+    d4.sort(my);
+    
+} 
+// 11.
+// 12.
